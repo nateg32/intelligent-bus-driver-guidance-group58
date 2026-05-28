15120e23be692720cdaa8eae7ed77c6b7f5f948d
nateg32 <s4169571@student.rmit.edu.au>
2026-05-28 15:24:23 +1000

    Clean up driver validation unit tests

--- src/test/java/au/edu/scc/busguidance/DriverUnitTest.java
+++ src/test/java/au/edu/scc/busguidance/DriverUnitTest.java
@@ -1,5 +1,6 @@
 package au.edu.scc.busguidance;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -8,135 +9,162 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class DriverUnitTest {
-    @Test
-    void validDriverIdIsAccepted() {
-        assertTrue(DriverValidator.isValidDriverID("23@@abCDXY"));
+    @Nested
+    class DriverIdFormatTests {
+        @Test
+        void validDriverIdIsAccepted() {
+            assertTrue(DriverValidator.isValidDriverID("23@@abCDXY"));
+        }
+
+        @Test
+        void nullDriverIdIsRejected() {
+            assertFalse(DriverValidator.isValidDriverID(null));
+        }
+
+        @Test
+        void driverIdShorterThanTenCharactersIsRejected() {
+            assertFalse(DriverValidator.isValidDriverID("23@@abCXY"));
+        }
+
+        @Test
+        void driverIdLongerThanTenCharactersIsRejected() {
+            assertFalse(DriverValidator.isValidDriverID("23@@abCDXYZ"));
+        }
+
+        @Test
+        void driverIdWithFirstDigitBelowTwoIsRejected() {
+            assertFalse(DriverValidator.isValidDriverID("13@@abCDXY"));
+        }
+
+        @Test
+        void driverIdWithZeroInFirstTwoDigitsIsRejected() {
+            assertFalse(DriverValidator.isValidDriverID("20@@abCDXY"));
+        }
+
+        @Test
+        void driverIdWithoutTwoSpecialCharactersInMiddleIsRejected() {
+            assertFalse(DriverValidator.isValidDriverID("23@aabCDXY"));
+        }
+
+        @Test
+        void driverIdWithWhitespaceAsSpecialCharacterIsRejected() {
+            assertFalse(DriverValidator.isValidDriverID("23@ abCDXY"));
+        }
+
+        @Test
+        void driverIdWithoutUppercaseFinalLettersIsRejected() {
+            assertFalse(DriverValidator.isValidDriverID("23@@abCDxy"));
+        }
     }
 
-    @Test
-    void driverIdShorterThanTenCharactersIsRejected() {
-        assertFalse(DriverValidator.isValidDriverID("23@@abCXY"));
+    @Nested
+    class UniqueDriverIdTests {
+        @Test
+        void uniqueDriverIdIsAccepted() {
+            List<Driver> existingDrivers = List.of(driver("23@@abCDXY"));
+
+            assertTrue(DriverValidator.isUniqueDriverID("24##efGHZZ", existingDrivers));
+        }
+
+        @Test
+        void duplicateDriverIdIsRejected() {
+            List<Driver> existingDrivers = List.of(driver("23@@abCDXY"));
+
+            assertFalse(DriverValidator.isUniqueDriverID("23@@abCDXY", existingDrivers));
+        }
+
+        @Test
+        void nullDriverIdIsNotUnique() {
+            List<Driver> existingDrivers = List.of(driver("23@@abCDXY"));
+
+            assertFalse(DriverValidator.isUniqueDriverID(null, existingDrivers));
+        }
+
+        @Test
+        void nullExistingDriverListIsNotUnique() {
+            assertFalse(DriverValidator.isUniqueDriverID("23@@abCDXY", null));
+        }
     }
 
-    @Test
-    void driverIdLongerThanTenCharactersIsRejected() {
-        assertFalse(DriverValidator.isValidDriverID("23@@abCDXYZ"));
+    @Nested
+    class AddressValidationTests {
+        @Test
+        void validAddressIsAccepted() {
+            assertTrue(DriverValidator.isValidAddress("12|King Street|Melbourne|VIC|Australia"));
+        }
+
+        @Test
+        void nullAddressIsRejected() {
+            assertFalse(DriverValidator.isValidAddress(null));
+        }
+
+        @Test
+        void addressMissingFieldIsRejected() {
+            assertFalse(DriverValidator.isValidAddress("12|King Street|Melbourne|VIC"));
+        }
+
+        @Test
+        void addressUsingWrongSeparatorIsRejected() {
+            assertFalse(DriverValidator.isValidAddress("12,King Street,Melbourne,VIC,Australia"));
+        }
+
+        @Test
+        void addressWithBlankFieldIsRejected() {
+            assertFalse(DriverValidator.isValidAddress("12||Melbourne|VIC|Australia"));
+        }
+
+        @Test
+        void addressWithNonNumericStreetNumberIsRejected() {
+            assertFalse(DriverValidator.isValidAddress("Twelve|King Street|Melbourne|VIC|Australia"));
+        }
+
+        @Test
+        void addressContainingSemicolonIsRejected() {
+            assertFalse(DriverValidator.isValidAddress("12|King Street|Melbourne|VIC|Australia;"));
+        }
     }
 
-    @Test
-    void driverIdWithFirstDigitBelowTwoIsRejected() {
-        assertFalse(DriverValidator.isValidDriverID("13@@abCDXY"));
-    }
+    @Nested
+    class BirthdateValidationTests {
+        @Test
+        void validBirthdateIsAccepted() {
+            assertTrue(DriverValidator.isValidBirthdate("15-04-1999"));
+        }
 
-    @Test
-    void driverIdWithZeroInFirstTwoDigitsIsRejected() {
-        assertFalse(DriverValidator.isValidDriverID("20@@abCDXY"));
-    }
+        @Test
+        void nullBirthdateIsRejected() {
+            assertFalse(DriverValidator.isValidBirthdate(null));
+        }
 
-    @Test
-    void driverIdWithoutTwoSpecialCharactersInMiddleIsRejected() {
-        assertFalse(DriverValidator.isValidDriverID("23@aabCDXY"));
-    }
+        @Test
+        void birthdateUsingWrongFormatIsRejected() {
+            assertFalse(DriverValidator.isValidBirthdate("1999-04-15"));
+        }
 
-    @Test
-    void driverIdWithWhitespaceAsSpecialCharacterIsRejected() {
-        assertFalse(DriverValidator.isValidDriverID("23@ abCDXY"));
-    }
+        @Test
+        void impossibleBirthdateIsRejected() {
+            assertFalse(DriverValidator.isValidBirthdate("31-02-1999"));
+        }
 
-    @Test
-    void driverIdWithoutUppercaseFinalLettersIsRejected() {
-        assertFalse(DriverValidator.isValidDriverID("23@@abCDxy"));
-    }
+        @Test
+        void birthdateWithInvalidMonthIsRejected() {
+            assertFalse(DriverValidator.isValidBirthdate("15-13-1999"));
+        }
 
-    @Test
-    void uniqueDriverIdIsAccepted() {
-        List<Driver> existingDrivers = List.of(driver("23@@abCDXY"));
+        @Test
+        void leapYearBirthdateIsAccepted() {
+            assertTrue(DriverValidator.isValidBirthdate("29-02-2000"));
+        }
 
-        assertTrue(DriverValidator.isUniqueDriverID("24##efGHZZ", existingDrivers));
-    }
+        @Test
+        void nonLeapYearFebruaryTwentyNinthIsRejected() {
+            assertFalse(DriverValidator.isValidBirthdate("29-02-2001"));
+        }
 
-    @Test
-    void duplicateDriverIdIsRejected() {
-        List<Driver> existingDrivers = List.of(driver("23@@abCDXY"));
-
-        assertFalse(DriverValidator.isUniqueDriverID("23@@abCDXY", existingDrivers));
-    }
-
-    @Test
-    void nullDriverIdIsNotUnique() {
-        List<Driver> existingDrivers = List.of(driver("23@@abCDXY"));
-
-        assertFalse(DriverValidator.isUniqueDriverID(null, existingDrivers));
-    }
-
-    @Test
-    void nullExistingDriverListIsNotUnique() {
-        assertFalse(DriverValidator.isUniqueDriverID("23@@abCDXY", null));
-    }
-
-    @Test
-    void validAddressIsAccepted() {
-        assertTrue(DriverValidator.isValidAddress("12|King Street|Melbourne|VIC|Australia"));
-    }
-
-    @Test
-    void addressMissingFieldIsRejected() {
-        assertFalse(DriverValidator.isValidAddress("12|King Street|Melbourne|VIC"));
-    }
-
-    @Test
-    void addressUsingWrongSeparatorIsRejected() {
-        assertFalse(DriverValidator.isValidAddress("12,King Street,Melbourne,VIC,Australia"));
-    }
-
-    @Test
-    void addressWithBlankFieldIsRejected() {
-        assertFalse(DriverValidator.isValidAddress("12||Melbourne|VIC|Australia"));
-    }
-
-    @Test
-    void addressWithNonNumericStreetNumberIsRejected() {
-        assertFalse(DriverValidator.isValidAddress("Twelve|King Street|Melbourne|VIC|Australia"));
-    }
-
-    @Test
-    void addressContainingSemicolonIsRejected() {
-        assertFalse(DriverValidator.isValidAddress("12|King Street|Melbourne|VIC|Australia;"));
-    }
-
-    @Test
-    void validBirthdateIsAccepted() {
-        assertTrue(DriverValidator.isValidBirthdate("15-04-1999"));
-    }
-
-    @Test
-    void birthdateUsingWrongFormatIsRejected() {
-        assertFalse(DriverValidator.isValidBirthdate("1999-04-15"));
-    }
-
-    @Test
-    void impossibleBirthdateIsRejected() {
-        assertFalse(DriverValidator.isValidBirthdate("31-02-1999"));
-    }
-
-    @Test
-    void birthdateWithInvalidMonthIsRejected() {
-        assertFalse(DriverValidator.isValidBirthdate("15-13-1999"));
-    }
-
-    @Test
-    void leapYearBirthdateIsAccepted() {
-        assertTrue(DriverValidator.isValidBirthdate("29-02-2000"));
-    }
-
-    @Test
-    void nonLeapYearFebruaryTwentyNinthIsRejected() {
-        assertFalse(DriverValidator.isValidBirthdate("29-02-2001"));
-    }
-
-    @Test
-    void birthdateWithSingleDigitDayIsRejected() {
-        assertFalse(DriverValidator.isValidBirthdate("5-04-1999"));
+        @Test
+        void birthdateWithSingleDigitDayIsRejected() {
+            assertFalse(DriverValidator.isValidBirthdate("5-04-1999"));
+        }
     }
 
     private Driver driver(String driverID) {
